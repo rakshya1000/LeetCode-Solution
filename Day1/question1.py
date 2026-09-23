5f2aa8ff3ca705476783682c914348cc32f6f177
rakshya1000 <neupaneraksha8@gmail.com>
2026-09-23 09:59:27 +0545

    day1
    easy question palindrome

---
+++ Day1/question1.py
@@ -0,0 +1,29 @@
+'''Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.'''
+
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        half = 0
+        while x > half:
+            half = half * 10 + x % 10
+            x //= 10
+        return x == half or x == half // 10
